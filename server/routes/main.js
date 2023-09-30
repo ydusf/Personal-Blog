e2510297fb1a096e2954fa5b865facec2798a6d9
sebastianlatham <LathamS2@cardiff.ac.uk>
2023-09-30 18:03:16 +0100

    index page css improved; post viewing added;

--- server/routes/main.js
+++ server/routes/main.js
@@ -10,12 +10,61 @@ router.get('', async (req, res) => {
       description: "Blog created using NodeJS, Express & MongoDB"
     }
 
-    const posts = await Post.find();
+    const data = await Post.find();
       
-    res.render('index', { locals, posts });
+    res.render('index', { locals, data });
   } catch (err) {
     console.log(err);
   }
 });
+
+// GET - ABOUT
+router.get('/about', async (req, res) => {
+  try {
+    const locals = {
+      title: "Personal Blog",
+      description: "Blog created using NodeJS, Express & MongoDB"
+    }
+      
+    res.render('about', { locals });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// GET - CONTACT
+router.get('/contact', async (req, res) => {
+  try {
+    const locals = {
+      title: "Personal Blog",
+      description: "Blog created using NodeJS, Express & MongoDB"
+    }
+      
+    res.render('contact', { locals });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// POST - CREATE-POST
+
+
+// GET - POST - ID
+router.get('/post/:id', async (req, res) => {
+  try {
+    const locals = {
+      title: "Personal Blog",
+      description: "Blog created using NodeJS, Express & MongoDB"
+    }
+
+    let slug = req.params.id
+
+    const data = await Post.findById({ _id: slug })
+      
+    res.render('post', { locals, data });
+  } catch (err) {
+    console.log(err);
+  }
+})
 
 module.exports = router;
