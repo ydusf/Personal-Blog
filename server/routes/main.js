b80e862e31dc8839c60e458425834e2f585ecfdf
sebastianlatham <LathamS2@cardiff.ac.uk>
2023-09-30 20:13:39 +0100

    create post, delete post added;

--- server/routes/main.js
+++ server/routes/main.js
@@ -2,6 +2,11 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/Post');
 
+const LOCALS = {
+  title: "Personal Blog",
+  description: "Blog created using NodeJS, Express & MongoDB"
+}
+
 // GET - HOME
 router.get('', async (req, res) => {
   try {
@@ -21,10 +26,7 @@ router.get('', async (req, res) => {
 // GET - ABOUT
 router.get('/about', async (req, res) => {
   try {
-    const locals = {
-      title: "Personal Blog",
-      description: "Blog created using NodeJS, Express & MongoDB"
-    }
+    const locals = LOCALS;
       
     res.render('about', { locals });
   } catch (err) {
@@ -35,10 +37,7 @@ router.get('/about', async (req, res) => {
 // GET - CONTACT
 router.get('/contact', async (req, res) => {
   try {
-    const locals = {
-      title: "Personal Blog",
-      description: "Blog created using NodeJS, Express & MongoDB"
-    }
+    const locals = LOCALS;
       
     res.render('contact', { locals });
   } catch (err) {
@@ -46,16 +45,75 @@ router.get('/contact', async (req, res) => {
   }
 });
 
+// GET - CREATE-POST
+router.get('/create-post', async (req, res) => {
+  try {
+    const locals = LOCALS;
+
+    res.render('create-post', { locals });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
 // POST - CREATE-POST
+router.post('/create-post', async (req, res) => {
+  try {
+    await Post.create({ title: req.body.title, body: req.body.body })
+
+    res.redirect('/');
+  } catch (err) {
+    console.log(err);
+  }
+})
+
+// GET - EDIT POST - :id
+router.put('/edit-post/:id', async (req, res) => {
+  try {
+    let slug = req.params.id
 
 
-// GET - POST - ID
+
+    res.redirect('/edit-post');
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// PUT - EDIT POST - :id
+router.put('/edit-post/:id', async (req, res) => {
+  try {
+    let slug = req.params.id
+
+    await Post.findByIdAndUpdate(slug, {
+      title: req.body.title,
+      body: req.body.body,
+      updatedAt: new Date.now()
+    });
+
+    res.redirect('/');
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// DELETE - DELETE POST - :id
+router.delete('/delete-post/:id', async (req, res) => {
+  try {
+    let slug = req.params.id
+
+    await Post.deleteOne( { _id: slug } );
+
+    res.redirect('/');
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// GET - POST - :id
 router.get('/post/:id', async (req, res) => {
   try {
-    const locals = {
-      title: "Personal Blog",
-      description: "Blog created using NodeJS, Express & MongoDB"
-    }
+    const locals = LOCALS;
 
     let slug = req.params.id
 
