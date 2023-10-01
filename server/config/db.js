e307c40a0e6c237cfe992cbb61818f8735da519b
sebastianlatham <LathamS2@cardiff.ac.uk>
2023-10-01 15:28:40 +0100

    db connection updated;

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 const connectDB = async ()=> {
     try {
         mongoose.set('strictQuery', false);
-        const conn = await mongoose.connect(process.env.MONGODB_URI);
+        const conn = await mongoose.connect(process.env.MONGODB_URI, {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+        });
         console.log(`Database Connected: ${conn.connection.host}`);
     } catch (error) {
         console.log(error);
