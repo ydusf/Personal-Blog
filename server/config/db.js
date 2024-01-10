7ef9b6854b4b4f23d1ebf1139363403b68927657
sebastianlatham <LathamS2@cardiff.ac.uk>
2024-01-10 14:04:43 +0000

    text colour changed to fix contrast

--- server/config/db.js
+++ server/config/db.js
@@ -1,15 +1,15 @@
-const mongoose = require('mongoose');
-const connectDB = async ()=> {
-    try {
-        mongoose.set('strictQuery', false);
-        const conn = await mongoose.connect(process.env.MONGODB_URI, {
-          useNewUrlParser: true,
-          useUnifiedTopology: true,
-        });
-        console.log(`Database Connected: ${conn.connection.host}`);
-    } catch (error) {
-        console.log(error);
-    }
-}
+const mongoose = require("mongoose");
+const connectDB = async () => {
+  try {
+    mongoose.set("strictQuery", false);
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`Database Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 module.exports = connectDB;
